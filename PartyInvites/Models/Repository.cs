bb087db16e435457bbd9dc535764e340423b7245
Kirill <blayner0027@gmail.com>
2021-12-27 22:18:45 +0400

    Добавил класс Repository

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,11 @@
+﻿namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static readonly List<GuestResponse> _guestResponses = new();
+
+        public static IEnumerable<GuestResponse> Responses() => _guestResponses;
+
+        public static void Add(GuestResponse guest) => _guestResponses.Add(guest);
+    }
+}
